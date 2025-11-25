c2d39ef1d805fc579f88f614cc45c25af32db0ba
akihiko.kigure <akihiko.kigure@gmail.com>
2025-11-25 23:33:42 +0900

    build: migrate v19.x

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,9 +5,10 @@ import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss'],
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.scss'],
+    standalone: false
 })
 export class HeroesComponent implements OnInit, OnDestroy {
   private destory = new Subject<void>();
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 import { MessagesService } from '../messages.service';
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss'],
+    selector: 'app-messages',
+    templateUrl: './messages.component.html',
+    styleUrls: ['./messages.component.scss'],
+    standalone: false
 })
 export class MessagesComponent implements OnInit {
   constructor(public messagesService: MessagesService) {}
