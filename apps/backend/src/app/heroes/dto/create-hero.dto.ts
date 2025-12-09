0ab0fc2d8fa7f4be163963f0a9126c9d5d0920b2
akihiko.kigure <akihiko.kigure@gmail.com>
2025-12-10 08:46:42 +0900

    fix: remove class-validator decorators (not installed)

--- apps/backend/src/app/heroes/dto/create-hero.dto.ts
+++ apps/backend/src/app/heroes/dto/create-hero.dto.ts
@@ -1,9 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateHeroDto {
   @ApiProperty({ description: 'Hero name', example: 'Dr. Nice' })
-  @IsString()
-  @IsNotEmpty()
   name: string;
 }
